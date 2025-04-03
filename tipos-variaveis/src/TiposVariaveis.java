d8a797a57f9ac8d0b92cd4580bf00eea17b3e6ab
JoaoV-A01 <victor.joao01101010@gmail.com>
2025-04-02 22:15:39 -0300

    estudando sobre tipos primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        double salario = 1250.70;
+
+        short numeroCurto = 2131;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // Casting, conversão de int para short
+    
+        int numero = 3;
+
+        numero = 2;
+
+        final double VALOR_DE_PI = 3.14; // Constante, não pode ser alterada
+
+        System.out.println(VALOR_DE_PI);
+
+        //tipos referencia
+        Sting nome = "Lucas";
     }
 }
